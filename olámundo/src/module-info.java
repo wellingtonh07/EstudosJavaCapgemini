e1298ce02b2470389ca83ccfd363252afbdfdd88
wellingtonh07 <wellington-holanda@hotmail.com>
2023-02-12 21:58:30 -0300

    olá mundo

---
+++ olámundo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Wellington
+ *
+ */
+module olámundo {
+}
---
+++ olámundo/src/olámundo/Principal.java
@@ -0,0 +1,16 @@
+package olámundo;
+
+public class Principal {
+	
+	//Início e fim de programa
+	public static void main(String[] args) {
+		
+		System.out.println("Olá mundo");
+		System.out.println("O que você escrever usando esse comando, será mostrado no console.");
+	
+	}
+}
+
+// Comentário de uma linha apenas
+/* Comentário de quantas linhas quiser */
+/** Comentário de documentação */
